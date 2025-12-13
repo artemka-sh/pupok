e24c145e2f3c39246b350af2dd2dd67732e106e2
artemka-sh <artemmmchik@mail.ru>
2025-12-14 01:05:10 +0300

    Чёткий разноцветный фрактал жюлиа с надписями.

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@
 
 using namespace cv;
 
-int MAX_ITERATIONS = 100;
+int MAX_ITERATIONS = 700;
 
-const double RE_START = -2.0;
-const double RE_END = 2.0;
-const double IM_START = -2.0;
-const double IM_END = 2.0;
+const double RE_START = -1.0;
+const double RE_END = 1.0;
+const double IM_START = -1.0;
+const double IM_END = 1.0;
 
 
 
@@ -40,11 +40,6 @@ public:
             double map_x = RE_START + (x / (double)m_img.cols) * (RE_END - RE_START);
             double map_y = IM_START + (y / (double)m_img.rows) * (IM_END - IM_START);
 
-            // if (calculate_pixel(map_y, map_x, m_C)) {
-            //    m_img.at<uchar>(y, x) = 0; // Точка внутри множества (черный)
-            // } else {
-            //    m_img.at<uchar>(y, x) = 255; // Точка улетела (белый)
-            // }
             m_img.at<uchar>(y, x) = calculate_pixel(map_y, map_x, m_C);
         }
     }
@@ -56,7 +51,10 @@ public:
         for (int i : std::views::iota(0, MAX_ITERATIONS))
         {
             if (std::norm(z) > 4.0)
-                return 255/std::norm(z);
+            {
+                // double smooth = i + 1 - std::log(std::log(std::abs(z))) / std::log(2.0);
+                return static_cast<uchar>(255 * i / MAX_ITERATIONS);
+            }
 
             z = std::pow(z, 5) - C + std::complex<double>(0.0, 0.003);
         }
@@ -68,20 +66,30 @@ public:
 
 int main( int argc, char** argv )
 {
-    int SIZE_X = 14280, SIZE_Y = 14280;
+    int SIZE_X = 16280, SIZE_Y = 16280;
     cv::namedWindow("Winda", cv::WINDOW_NORMAL);
     cv::Mat_<uchar> juliaImg(SIZE_X, SIZE_Y);
+    cv::Mat colorJuliaImg(SIZE_X, SIZE_Y, CV_8UC3);
+    cv::Mat normalized;
 
     // for (double C = -0.6099999999999996 ; C <= -0.4999999; C+=0.005)
     double C = 0.549653;
     {
-
+        std::print("displaying... {} \n.", C);
         JuliaCalculator parallelJulia(juliaImg, 0, 0, SIZE_X, SIZE_Y, C);
         parallel_for_(Range(0, juliaImg.rows*juliaImg.cols), parallelJulia);
 
+        cv::normalize(juliaImg, normalized, 0, 255, cv::NORM_MINMAX, CV_8U);
+        cv::applyColorMap(normalized, colorJuliaImg, cv::COLORMAP_TURBO ); //exception
+        cv::putText(colorJuliaImg, "Lubu", cv::Point2i(1414, 7279), cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 1.0, cv::Scalar(255, 255, 255, 0));
+        cv::putText(colorJuliaImg, "Prilubu", cv::Point2i(1414, 7319), cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 1.0, cv::Scalar(255, 255, 255, 0));
+        cv::putText(colorJuliaImg, "KIRA, I LOVE YOU!", cv::Point2i(12712, 10753), cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 1.0, cv::Scalar(255, 255, 255, 0));
+        cv::putText(colorJuliaImg, "much ^*^", cv::Point2i(12712, 10793), cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 1.0, cv::Scalar(255, 255, 255, 0));
 
-        std::print("displaying... {} \n.", C);
-        cv::imshow("Winda", juliaImg);
+        cv::imshow("Winda", colorJuliaImg);
+
+        imwrite("julia_gray.jpg", juliaImg);
+        imwrite("julia.jpg", colorJuliaImg);
         auto key = cv::waitKey(1);
 
         if (key == 'q') {
@@ -89,6 +97,11 @@ int main( int argc, char** argv )
         }
     }
     waitKey(0);
+
+
+    double minVal, maxVal;
+    cv::minMaxLoc(juliaImg, &minVal, &maxVal);
+    std::print("min: {}, max: {}", minVal, maxVal);
     cv::destroyAllWindows();
 
 }
